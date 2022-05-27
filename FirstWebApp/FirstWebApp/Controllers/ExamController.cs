4697d8df4c22a299ace56583f3cceeaa5e6119bd
Lamie2 <7metelt@code.edu.az>
2022-05-27 11:58:50 +0400

    first commit

---
+++ FirstWebApp/FirstWebApp/Controllers/ExamController.cs
@@ -0,0 +1,27 @@
+﻿using FirstWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Controllers
+{
+    public class ExamController:Controller
+    {
+        private List<Exam> _exams;
+        public ActionResult Result(int studentId=19)
+        {
+            return Content(studentId.ToString());
+            //return View();
+        }
+        public ActionResult Detail(int id)
+        {
+            TempData["ExamId"] = id;
+            ViewData["ExamId"] = id;
+            ViewBag.ExamId = id;
+            return View();
+
+        }
+    }
+}
---
+++ FirstWebApp/FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ContentResult Index()
+        {
+            return Content("Salam dunya");
+        }
+
+        public ViewResult About()
+        {
+            var view = new ViewResult();
+            view.ViewName = "About";
+            return view;
+        }
+
+        public ViewResult Center()
+        {
+            return View();
+        }
+
+
+    }
+}
